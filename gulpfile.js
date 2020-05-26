c46040f5d5f0863adaf9dbec81f66ede229a4fda
Marcin Kalinowski <m.kalinowski@intive.com>
2020-05-26 14:43:19 +0200

    gulp styles compiler config + integrate with theme .yml

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+// const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+// const browserSync = require('browser-sync').create();
 
-const FOLDERS = {
-  PROJECT: 'meamo_2020',
-  THEME: 'themes/meamo',
-  HTDOCS_MAC: '/Users/marcin.kalinowski/.bitnami/stackman/machines/xampp/volumes/root/htdocs/'
-};
+const PROJECT_NAME = 'meamo_2020';
+const THEME_FOLDER = './themes/meamo';
+const THEME_ASSETS = `${THEME_FOLDER}/**/*`;
+
+const HTDOCS_MAC = '/Users/marcin.kalinowski/.bitnami/stackman/machines/xampp/volumes/root/htdocs/';
+const THEME_DEPLOY_PATH = `${HTDOCS_MAC}/${PROJECT_NAME}/${THEME_FOLDER}`;
 
 const CONFIG = {
   SRC: {
@@ -12,41 +19,58 @@ const CONFIG = {
       // './src/js/*.js',
     ],
     SCSS: [
-      // './src/scss/main.scss',
+      `${THEME_FOLDER}/src/scss/main.scss`
       ],
     CSS: [
       // './src/css/**/*.css'
     ],
-    THEME: `./${FOLDERS.THEME}/**/*`
+    THEME: `${THEME_FOLDER}/**/*`
   },
   DIST: {
-    THEME: `${FOLDERS.HTDOCS_MAC}/${FOLDERS.PROJECT}/${FOLDERS.THEME}`
+    FILENAME_STYLES: `meamo.min.css`,
+    FILENAME_SCRIPTS: `meamo.min.js`,
+    FOLDER_STYLES: `${THEME_FOLDER}/assets/css`,
+    FOLDER_SCRIPTS: `${THEME_FOLDER}/assets/js`,
   },
   WATCHERS: {
     SCSS: [
-      // './src/scss/**/*.scss'
+      `${THEME_FOLDER}/src/scss/**/*.scss`
     ],
     THEME: [
-      `./${FOLDERS.THEME}/**/*`
+      `${THEME_FOLDER}/**/*`
     ]
   }
 };
 
+const StylesTaskDev = done => {
+  // TODO: css vendors should be imported from node_modules
+  gulp.src([...CONFIG.SRC.CSS, ...CONFIG.SRC.SCSS])
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  // .on('error', swallowError)
+  .pipe(concat(CONFIG.DIST.FILENAME_STYLES))
+  // .pipe(autoprefixer({browsers: ['last 2 versions', '> 1% in PL', 'ie >=10']}))
+  .pipe(cleanCSS())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(CONFIG.DIST.FOLDER_STYLES));
+  done();
+}
+
 const deployToXamppMac = done => {
     gulp.src(CONFIG.SRC.THEME)
-    .pipe(gulp.dest(CONFIG.DIST.THEME))
+    .pipe(gulp.dest(THEME_DEPLOY_PATH))
     done();
 };
 
 function watchers() {
-  // gulp.watch(config.watchers.scss, series(StylesTask, reload));
+  gulp.watch(CONFIG.WATCHERS.SCSS, gulp.series(StylesTaskDev, deployToXamppMac));
   // gulp.watch(config.src.css, series(StylesTask, reload));
   // gulp.watch(config.src.js, series(JsTask, reload));
-  gulp.watch(CONFIG.WATCHERS.THEME, deployToXamppMac);
+  // gulp.watch(CONFIG.WATCHERS.THEME, deployToXamppMac);
 }
 
 exports.default = gulp.parallel(
-  // StylesTask,
+  StylesTaskDev,
   // JsTask,
   deployToXamppMac,
   watchers,
