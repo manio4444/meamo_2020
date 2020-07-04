96655602e68ea760162313fe87fdc66b60c546da
manio4444 <kalinowskixmarcin@o2.pl>
2020-07-05 01:25:01 +0200

    instagram section

--- themes/meamo/src/js/instagram/gallery-fetch.js
+++ themes/meamo/src/js/instagram/gallery-fetch.js
@@ -2,25 +2,54 @@ import $ from 'jquery';
 window.$ = $;
 window.jQuery = $;
 
-const token = 'your access token'; // learn how to obtain it below
-const userName = 'rudrastyh'; // rudrastyh - my username :)
-const num_photos = 4; // how much photos do you want to get
+import Instafeed from 'instafeed.js';
 
-$.ajax({
-  url: 'https://api.instagram.com/v1/users/search',
-	dataType: 'jsonp',
-	type: 'GET',
-  data: {access_token: token, q: userName}, // actually it is just the search by username
-	success: data => {
- 		console.log(data);
-		// for( x in data.data ){
-		// 	$('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
-		// 	// data.data[x].images.thumbnail.url - URL of image 150х150
-		// 	// data.data[x].images.standard_resolution.url - URL of image 612х612
-		// 	// data.data[x].link - Instagram post URL
-		// }
-	},
-	error: data => {
-		console.log(data); // send the error notifications to console
-	}
+const sliderConfig = {
+  variableWidth: true,
+  arrows: false,
+  infinite: true,
+  // prevArrow: '<span class="slick-prev material-icons">keyboard_arrow_left</span>',
+  // nextArrow: '<span class="slick-next material-icons">keyboard_arrow_right</span>',
+  rows: 0, // fix for not adding <div> wrapper inside slide's
+};
+
+$(document).ready(function() {
+
+  const $constinstagramSlider = $('.section-instagram #instafeed');
+  const instagramUserId = '17841400647940820';
+  const instagramAccessToken = 'IGQVJWcHIzSUpZAZADZAFd3BNOEx3R0ZA2ajhFNENSTUx4dVNzd0FZAeEUxWU1yR08tcTBfeURIVEN6aFNYNU04TEJvdXV4X3FhRlRvSFUxLU1UMm5iNmUzS0JjVXBEV1lkQ1pMVHVLQXFYQ0NUaWVTT0pIaQZDZD';
+
+  if ($constinstagramSlider.length === 0) {
+    return;
+    console.log('##################### return ###################');
+  }
+
+  $.ajax({
+    method: "GET",
+    url: `https://graph.instagram.com/${instagramUserId}/media`,
+    data: {
+      access_token: instagramAccessToken,
+      fields: 'media_url,permalink',
+      size: 'm'
+    }
+  })
+  .done(function(data) {
+    const images = data.data.map(item => item.permalink);
+
+    images.forEach(imageUrl => {
+      $('<img>')
+      .attr('src', `${imageUrl}media/?size=m`)
+      .appendTo($constinstagramSlider);
+    });
+
+console.log($constinstagramSlider.find('img'));
+    $constinstagramSlider.find('img').each(function() {
+      $(this)
+      .wrap('<li>')
+      .wrap('<a href="#">');
+    });
+
+    $constinstagramSlider.slick(sliderConfig);
+  });
+
 });
---
+++ themes/meamo/templates/catalog/_partials/instagram-section.tpl
@@ -0,0 +1,20 @@
+<section class="section-instagram">
+
+  <ul id="instafeed"></ul>
+  <div class="lds-roller section-instagram__spinner">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+
+  <div class="meamo-title-normal section-instagram__title">
+    {l s='Follow us on instagram' d='Shop.Theme.Instagram'}
+    <a href="#">@meamoofficial</a>
+  </div>
+
+</section>
