65b238675968a24eb3241746fa1c157d11b72dbd
manio4444 <kalinowskixmarcin@o2.pl>
2020-07-13 10:56:25 +0200

    code clean

--- themes/meamo/src/js/product/semantic-select.js
+++ themes/meamo/src/js/product/semantic-select.js
@@ -6,11 +6,7 @@ import 'semantic-ui-dropdown/dropdown.min.js';
 import 'semantic-ui-transition/transition.min.js';
 
 $(document).ready(function() {
-  const $lookbook_container = $('.meamo-lookbook');
-  let counter = 1;
 
-  $('.ui.dropdown')
-    .dropdown()
-  ;
+  $('.ui.dropdown').dropdown();
 
 });
