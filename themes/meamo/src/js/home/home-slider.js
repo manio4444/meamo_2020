63ef456b8b1fd8bdaf7cca63dfd1e0c78f017d46
manio4444 <kalinowskixmarcin@o2.pl>
2020-07-15 13:08:03 +0200

    bolder second row on slide home and enable pauseOnHover fetching from BO

--- themes/meamo/src/js/home/home-slider.js
+++ themes/meamo/src/js/home/home-slider.js
@@ -7,25 +7,29 @@ import 'slick-carousel';
 
 $(document).ready(function() {
 
-  const sliderDestkop = $('.home_slider--init.desktop');
-  const sliderMobile = $('.home_slider--init.mobile');
+  const $sliderDestkop = $('.home_slider--init.desktop');
+  const $sliderMobile = $('.home_slider--init.mobile');
+
+  const $sliderToShow = $sliderDestkop; //temp
+  const autoplaySpeed = $sliderToShow.attr('data-interval');
+  const pauseOnHover = ($sliderToShow.attr('data-pause')  == 'hover') ? true : false;
 
   // if (window.matchMedia("(orientation: portrait)").matches) {
-  //   const sliderToShow = sliderMobile;
-  //   sliderDestkop.hide();
+  //   const $sliderToShow = $sliderMobile;
+  //   $sliderDestkop.hide();
   // } else {
-  //   const sliderToShow = sliderDestkop;
-  //   sliderMobile.hide();
+  //   const $sliderToShow = $sliderDestkop;
+  //   $sliderMobile.hide();
   // }
-  const sliderToShow = sliderDestkop; //temp
 
-  sliderToShow.slick({
+  $sliderToShow.slick({
     // centerMode: true,
     // variableWidth:true,
     arrows: false,
     dots: true,
     autoplay: true,
-    autoplaySpeed: sliderToShow.attr('data-interval'),
+    autoplaySpeed,
+    pauseOnHover,
     rows: 0, // fix for not adding <div> wrapper inside slide's
     // infinite: false,
     // asNavFor: $('#section_realization .r_slider_sub'),
