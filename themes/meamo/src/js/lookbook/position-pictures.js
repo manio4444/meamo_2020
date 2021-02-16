96fa7ba36b57e3dfb9d612952e4bac4acec09932
manio4444 <kalinowskixmarcin@o2.pl>
2021-02-16 01:56:51 +0100

    lookbook enable wrap <img> into <a>

--- themes/meamo/src/js/lookbook/position-pictures.js
+++ themes/meamo/src/js/lookbook/position-pictures.js
@@ -4,29 +4,41 @@ window.jQuery = $;
 
 $(document).ready(function() {
   const $lookbook_container = $('.meamo-lookbook');
+  const $lookbook_pictures_temp = $('.meamo-lookbook__pictures-temp');
   let counter = 1;
 
-  $lookbook_container.find('img').each(function() {
+  function moveElements() {
+    $lookbook_pictures_temp.find('.meamo-lookbook__image-section').each(function() {
+      $(this)
+      .appendTo($lookbook_container)
+      .addClass('counter-' + counter);
+
+      if ($(this).is(':nth-child(4n+1')) {
+        $(this).addClass('padding');
+      }
+
+      if ($(this).is(':nth-child(4n+2')) {
+        $(this).addClass('padding');
+      }
+
+      counter++;
+    });
+  }
+
+  $lookbook_pictures_temp.find('a').has('img').each(function() {
     $(this)
     .wrap('<div class="meamo-lookbook__image-section"></div>');
   });
 
-  $('.meamo-lookbook__image-section').each(function() {
+  moveElements();
+
+  $lookbook_pictures_temp.find('img').each(function() {
     $(this)
-    .appendTo($lookbook_container)
-    .addClass('counter-' + counter);
-
-    if ($(this).is(':nth-child(4n+1')) {
-      $(this).addClass('padding');
-    }
-
-    if ($(this).is(':nth-child(4n+2')) {
-      $(this).addClass('padding');
-    }
-
-    counter++;
+    .wrap('<div class="meamo-lookbook__image-section"></div>');
   });
 
-  $('.meamo-lookbook__pictures-temp').remove();
+  moveElements();
+
+  $lookbook_pictures_temp.remove();
 
 });
